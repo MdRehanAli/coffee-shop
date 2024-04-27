f56602c3cc7a2a4b6d777331f5a2620b4ea1fdb0
MdRehanAli <rehanali21d@gmail.com>
2024-04-27 21:20:22 +0600

    added middleware

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000
 
-
+// Middlewares
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello from server')
